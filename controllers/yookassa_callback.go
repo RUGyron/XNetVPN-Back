63a047b3c171eb062f68ba92af0f7b5818ee0c78
RUGyron <pivosh098@gmail.com>
2025-04-20 18:40:50 +0400

    debug

--- controllers/yookassa_callback.go
+++ controllers/yookassa_callback.go
@@ -1,19 +1,28 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
 )
 
 func YookassaCallback(c *gin.Context) {
-	var body map[string]interface{}
-
-	if err := c.BindJSON(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid JSON"})
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to read body"})
 		return
 	}
 
-	fmt.Printf("YooKassa callback: %+v\n", body)
+	var pretty bytes.Buffer
+	if err := json.Indent(&pretty, body, "", "  "); err != nil {
+		// если не удалось отформатировать, просто выведем как есть
+		fmt.Println("Raw JSON:\n", string(body))
+	} else {
+		fmt.Println("Formatted JSON:\n", pretty.String())
+	}
+
 	c.Status(http.StatusOK)
 }
